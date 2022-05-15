dbe3cb31c38621be5e4c48d99f4ce642a056f44c
Ben Hopkins <13667722+benhopkinstech@users.noreply.github.com>
2022-05-15 15:54:13 +0100

    Added event handlers for dynamically created controls and fetched the "id" of the entry within the XML document in order to program the features

--- PerfectTablesMessageCentre/Form1.cs
+++ PerfectTablesMessageCentre/Form1.cs
@@ -1,3 +1,4 @@
+using System.Text.RegularExpressions;
 using System.Xml.Linq;
 
 namespace PerfectTablesMessageCentre
@@ -28,8 +29,9 @@ namespace PerfectTablesMessageCentre
 
                 foreach (XElement xe in xDoc.Descendants("Response"))
                 {
+                    string id = xe.Attribute("id").Value;
                     lstBox[i] = new ListBox();
-                    lstBox[i].Name = "txtMessage" + i;
+                    lstBox[i].Name = "txtMessage" + id;
                     foreach (string s in xe.Element("Message").Value.Split("\n"))
                     {
                         lstBox[i].Items.Add(s);
@@ -37,25 +39,28 @@ namespace PerfectTablesMessageCentre
                     lstBox[i].Location = new Point(12, 78 + space);
                     lstBox[i].Size = new Size(340, 55);
                     lbl[i] = new Label();
-                    lbl[i].Name = "lblTitle" + i;
+                    lbl[i].Name = "lblTitle" + id;
                     lbl[i].Text = xe.Element("Title").Value;
                     lbl[i].Location = new Point(12, 58 + space);
                     btnEdit[i] = new Button();
-                    btnEdit[i].Name = "btnEdit" + i;
+                    btnEdit[i].Name = "btnEdit" + id;
                     btnEdit[i].Text = "Edit Response";
                     btnEdit[i].Location = new Point(12, 139 + space);
                     btnEdit[i].Size = new Size(99, 27);
+                    btnEdit[i].Click += new EventHandler(btnEdit_Click);
                     btnDelete[i] = new Button();
-                    btnDelete[i].Name = "btnDelete" + i;
+                    btnDelete[i].Name = "btnDelete" + id;
                     btnDelete[i].Text = "Delete Response";
                     btnDelete[i].Location = new Point(238, 139 + space);
                     btnDelete[i].Size = new Size(114, 27);
+                    btnDelete[i].Click += new EventHandler(btnDelete_Click);
                     btnCopy[i] = new Button();
-                    btnCopy[i].Name = "btnCopy" + i;
+                    btnCopy[i].Name = "btnCopy" + id;
                     btnCopy[i].Text = "Copy";
                     btnCopy[i].Location = new Point(357, 78 + space);
                     btnCopy[i].Size = new Size(46, 55);
-              
+                    btnCopy[i].Click += new EventHandler(btnCopy_Click);
+
                     this.Controls.Add(lstBox[i]);
                     this.Controls.Add(lbl[i]);
                     this.Controls.Add(btnEdit[i]);
@@ -69,7 +74,21 @@ namespace PerfectTablesMessageCentre
 
             InitializeComponent();
         }
-
+        private void btnEdit_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int id = Int32.Parse(Regex.Match(button.Name, @"\d+").Value);
+        }
+        private void btnDelete_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int id = Int32.Parse(Regex.Match(button.Name, @"\d+").Value);
+        }
+        private void btnCopy_Click(object sender, EventArgs e)
+        {
+            Button button = sender as Button;
+            int id = Int32.Parse(Regex.Match(button.Name, @"\d+").Value);
+        }
         private void btnAddResponse_Click(object sender, EventArgs e)
         {
             frmAddResponse form = new frmAddResponse();
